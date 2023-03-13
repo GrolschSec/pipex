000b05a37927f258523577595629ca5f01d10025
Grolsch Information Security <grolsch-sec@protonmail.com>
2023-03-13 14:19:10 +0100

    Modifying error message function

--- srcs/error.c
+++ srcs/error.c
@@ -6,7 +6,7 @@
 /*   By: rlouvrie <rlouvrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 17:17:12 by rlouvrie          #+#    #+#             */
-/*   Updated: 2023/03/13 14:02:35 by rlouvrie         ###   ########.fr       */
+/*   Updated: 2023/03/13 14:17:07 by rlouvrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void	error_msg(char *name, char *message, int status)
 		ft_putstr_fd(name, 2);
 		ft_putstr_fd(": ", 2);
 		ft_putstr_fd(message, 2);
-		ft_putchar_fd('\n', 2);
 	}
 	else
 		perror(message);
